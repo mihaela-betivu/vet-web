279e390b78a387e4a31eddad9e7a22c3f016c736
Mihaela Betivu <mihaela.betivu@isa.utm.md>
2024-03-09 21:58:43 +0200

    Project init - add template, config bundle, layout, controller

---
+++ eUseControl.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,48 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace eUseControl.Web
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            //bootstrap
+            bundles.Add(new StyleBundle("~/bundles/bootstrap/css").Include("~/Content/bootstrap/bootstrap.min.css",
+                new CssRewriteUrlTransform()));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap/js").Include("~/Content/bootstrap/bootstrap.min.js"));
+
+            //google-map
+            bundles.Add(new ScriptBundle("~/bundles/google-map/js").Include("~/Content/google-map/gmap.js"));
+
+            //icofont-css
+            bundles.Add(new StyleBundle("~/bundles/icofont/css").Include("~/Content/icofont/icofont.min.css",
+                new CssRewriteUrlTransform()));
+
+            //jquery
+            bundles.Add(
+                new ScriptBundle("~/bundles/jquery/js").Include("~/Content/jquery/jquery.js"));
+
+            //shuffle
+            bundles.Add(new ScriptBundle("~/bundles/shuffle/js").Include("~/Content/shuffle/shuffle.min.js"));
+
+            //slick-carousel - css
+            bundles.Add(new StyleBundle("~/bundles/slick-carousel/css").Include(
+                "~/Content/slick-carousel/slick/slick.css",
+                "~/Content/slick-carousel/slick/slick-theme.css"));
+
+            //slick-carousel - js
+            bundles.Add(
+                new ScriptBundle("~/bundles/slick-carousel/js").Include("~/Content/slick-carousel/slick/slick.min.js"));
+
+            //assets - css
+            bundles.Add(
+                new StyleBundle("~/bundles/assets/css/style").Include("~/assets/css/style.css",
+                    new CssRewriteUrlTransform()));
+
+            //assets - js
+            bundles.Add(new ScriptBundle("~/bundles/assets/script/js").Include("~/assets/js/script.js"));
+        }
+    }
+}
---
+++ eUseControl.Web/Controllers/AppoimentController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eUseControl.Web.Controllers
+{
+    public class AppoimentController : Controller
+    {
+        public ActionResult Appoiment()
+        {
+            return View();
+        }
+    }
+}
